7c9e736a556cfabf640bedab39984956968575c4
Muhammad Saad <syedmuhammadsaad@live.com>
2021-01-11 20:51:28 +0500

    configured database setting, added 2 controllers, added core base controller and deleted default welcome controller

---
+++ application/controllers/admin.php
@@ -0,0 +1,14 @@
+<?php
+
+class Admin extends MY_Controller{
+
+    // Admin controller
+    public function index(){
+        echo "Admin Controller";
+    }
+
+}
+
+
+
+
---
+++ application/controllers/user.php
@@ -0,0 +1,13 @@
+<?php
+
+class User extends MY_Controller{
+
+    // Public controller
+    public function index(){
+        echo "Public Controller";
+    }
+
+
+}
+
+
